27f533ea7185f5ef4a0206c65538b7f4b3a01a9b
anasmohsen51132-star <anasmohsen51132@gmail.com>
2026-06-30 09:24:55 +0300

    Delete app directory

--- app/(admin)/loading.tsx
+++
@@ -1,7 +0,0 @@
-// src/app/(admin)/loading.tsx
-// NEXT-002 FIX: see (student)/loading.tsx for context.
-import { FullScreenSpinner } from "@/components/ui/FullScreenSpinner";
-
-export default function AdminLoading() {
-  return <FullScreenSpinner />;
-}
